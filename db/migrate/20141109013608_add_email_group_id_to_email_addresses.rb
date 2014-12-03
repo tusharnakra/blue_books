b0c05b1eb90c45aa6f1519329f8aa7829abff120
Tushar Nakra <tusharnakra@vagvlan567.0622.wlan.wireless-pennnet.upenn.edu>
2014-12-02 19:22:01 -0500

    destroyed unnecessary migrations

--- db/migrate/20141109013608_add_email_group_id_to_email_addresses.rb
+++
@@ -1,5 +0,0 @@
-class AddEmailGroupIdToEmailAddresses < ActiveRecord::Migration
-  def change
-    add_column :email_addresses, :email_group_id, :integer
-  end
-end
--- db/migrate/20141121213240_remove_email_group_id_to_email_addresses.rb
+++
@@ -1,9 +0,0 @@
-class RemoveEmailGroupIdToEmailAddresses < ActiveRecord::Migration
-  def up
-    remove_column :email_addresses, :email_group_id
-  end
-
-  def down
-    add_column :email_addresses, :email_group_id, :integer
-  end
-end
