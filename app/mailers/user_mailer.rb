9a55355b82969d905c87f8748372e5c049478948
Tushar Nakra <tusharnakra@Tushars-MacBook-Pro.local>
2014-12-16 22:08:14 -0500

    request access

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,10 +2,12 @@ class UserMailer < ActionMailer::Base
   def send_request(request_access)
 
     admin_id = Group.where(:name => "Admin").first[:id]
+    print admin_id
     emails = []
-    Member.where("group_id LIKE ?", admin_id).each do |table_entry|
+    Member.where(:group_id => admin_id).each do |table_entry|
       emails << table_entry[:email_address]
     end
+    print emails
 
     Mail.deliver do
       from    'fileshare597@gmail.com'
